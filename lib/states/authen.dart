98bbd0e0e8a3aee31f28c9a7eadcb536a2d9acaf
siancom <siancom@gmail.com>
2021-10-20 22:40:39 +0700

    clip6.1

---
+++ lib/states/authen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  const Authen({Key? key}) : super(key: key);
+
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('this is authen'),
+      ),
+    );
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'SpmWorkshop';
+  //Rout
+
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSellerService = 'sellerService';
+  static String routeRiderService = 'riderService';
+}
