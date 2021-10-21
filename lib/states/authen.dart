48a49223476ed33206abf9fd00a799594dd744ee
siancom <siancom@gmail.com>
2021-10-22 03:19:43 +0700

    ep9

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utility/my_constant.dart';
+import 'package:flutter_application_1/widgets/show_image.dart';
+import 'package:flutter_application_1/widgets/show_title.dart';
 
 class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
@@ -8,12 +13,131 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Text('this is authen'),
-      ),
+          child: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        behavior: HitTestBehavior.opaque,
+        child: ListView(
+          children: [
+            buildAppName(),
+            buildImage(size),
+            buildUser(size),
+            buildPassword(size),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+//password Textbox
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye
+                    ? Icon(
+                        Icons.remove_red_eye,
+                        color: MyConstant.dark,
+                      )
+                    : Icon(
+                        Icons.remove_red_eye_outlined,
+                        color: MyConstant.dark,
+                      ),
+              ),
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'Password:',
+              prefixIcon: Icon(
+                Icons.vpn_key_outlined,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.ligth),
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+//userTextbox
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'User:',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.ligth),
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().h1Style(),
+        ),
+      ],
+    );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(path: MyConstant.image4),
+        ),
+      ],
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -10,4 +10,30 @@ class MyConstant {
   static String routeBuyerService = 'buyerService';
   static String routeSellerService = 'sellerService';
   static String routeRiderService = 'riderService';
+  //image
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+  //color
+  static Color primary = Color(0xff00bfa5);
+  static Color dark = Color(0xff008e76);
+  static Color ligth = Color(0xff5df2d6);
+
+  //style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
 }
